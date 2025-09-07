adba13f6dabcf75496282e858881130365b7c3b9
Edgar Cruz <Edg_const@hotmail.com>
2025-09-06 23:23:51 -0600

    Modificación de botones de ciudades importantes para poder seleccionarlas desde su parte inferior de la pantalla

--- app/Components/TopCities/TopCities.tsx
+++ app/Components/TopCities/TopCities.tsx
@@ -2,15 +2,27 @@
 
 import defaultStates from '@/app/Utils/defaultStates';
 import React from 'react';
+import { useGlobalContextUpdate } from '@/app/Context/globalContext';
 
 export default function TopCities() {
+  const { setActiveCityCoords } = useGlobalContextUpdate();
+  
+    const getClickedCoords = (lat: number, lon: number) => {
+      setActiveCityCoords( [ lat, lon ] );
+    };
   return (
     <div className='states flex flex-col gap-3 flex-1'>
       <h2 className='text-center font-medium'>Ciudades Principales</h2>
       <div className='flex flex-col gap-3'>
         { defaultStates.map( ( state, index ) => {
           return (
-            <div key={ index } className='border rounded-lg cursor-pointer dark:bg-dark-gray shadow-sm dark:shadow-none'>
+            <div 
+              key={ index } 
+              className='border rounded-lg cursor-pointer dark:bg-dark-gray shadow-sm dark:shadow-none'
+              onClick={ () => {
+                getClickedCoords( state.lat, state.lon );
+              }}
+            >
               <p className='px-6 py-3'>{ state.name }</p>
             </div>
           )
